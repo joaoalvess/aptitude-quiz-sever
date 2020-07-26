2e44008ac8354156cdceaf9d2ce3a109daa421ab
Elcoss <jp14700@gmail.com>
2020-07-26 02:13:35 -0300

    database ajust migrations

--- src/database/migrations/01_create_forms.ts
+++ src/database/migrations/01_create_forms.ts
@@ -3,11 +3,11 @@ import Knex from 'knex'
 export async function up(knex: Knex) {
   return knex.schema.createTable('forms', table => {
     table.increments('id').primary();
+    table.boolean('contato_infectado')
     table.date('data')
-    table.boolean('contato com infectado')
     table.boolean('tosse')
     table.boolean('febre')
-    table.boolean('fata de ar')
+    table.boolean('falta_ar')
     table.boolean('calafrio')
     table.decimal('temperatura')
 
