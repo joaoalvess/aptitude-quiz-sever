32933385cc1e732700a29af6d2969bd669952423
Elcoss <jp14700@gmail.com>
2020-07-26 02:14:17 -0300

    add forms routes and user routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,18 @@
 import express from "express";
 
 import UsersController from './controllers/UsersController'
+import FormsController from './controllers/FormsController'
 
 const routes = express.Router();
 const usersController = new UsersController()
+const formsController = new FormsController()
 
 routes.post("/createuser", usersController.create);
+routes.get("/users", usersController.index);
+routes.get("/user/:id", usersController.show);
+
+routes.post("/createform/:id", formsController.create);
+routes.get("/form/:id", formsController.show);
+routes.get("/form", formsController.index);
 
 export default routes;
