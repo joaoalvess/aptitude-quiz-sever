d227b27d6d99004c6b9656684672a7e9df159cd9
Elcoss <jp14700@gmail.com>
2020-07-27 22:26:43 -0300

    create form controller

--- src/routes.ts
+++ src/routes.ts
@@ -2,14 +2,18 @@ import express from "express";
 
 import UsersController from './controllers/UsersController'
 import FormsController from './controllers/FormsController'
+import TodayController from './controllers/TodayController'
 
 const routes = express.Router();
 const usersController = new UsersController()
 const formsController = new FormsController()
+const todayController = new TodayController()
 
 routes.post("/createuser", usersController.create);
 routes.get("/users", usersController.index);
 routes.get("/user/:id", usersController.show);
+
+routes.get("/formtoday/:id", todayController.show);
 
 routes.post("/createform/:id", formsController.create);
 routes.get("/form/:id", formsController.show);
